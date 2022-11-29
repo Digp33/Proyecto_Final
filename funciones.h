050ed143a825fdb2ea301baf3da81d3300000bdd
Digp33 <85522284+Digp33@users.noreply.github.com>
2022-11-29 17:02:35 -0600

    Update funciones.h
    Se cambian los int por void en las funciones

--- funciones.h
+++ funciones.h
@@ -1,10 +1,10 @@
 #define G 3
 #define N 10
 
-int funTriangulo(int x);
-int funCuadrilateros(int x);
-int funConversion(int x);
-int funFactorial(int x) ;
+void funTriangulo(int x);
+void funCuadrilateros(int x);
+void funConversion(int x);
+void funFactorial(int x) ;
 void leer_matriz(int datoUno[N][N], int F, int C);
 void imprimir_matriz(int datoUno[N][N],int F,int C);
 void multiplicar(int datoUno[N][N],int datoDos[N][N],int resultadoM[N][N],int f1,int cf,int c2);
@@ -12,7 +12,7 @@ void suma_matriz(int datoUno[N][N],int datoDos[N][N],int matrizResultado[N][N],i
 void transpuesta(int datoUnoT[G][G],int datoDosT[G][G],int espacio);
 void leer_matrizTrans(int datoUnoT[G][G], int F, int C);
 void imprimir_matrizTrans(int datoUnoT[G][G],int F,int C);
-int funSumaMatriz(int x);
-int funMultiMatriz(int x);
-int funTransMatriz(int x);
-int funSalario(int x);
+void funSumaMatriz(int x);
+void funMultiMatriz(int x);
+void funTransMatriz(int x);
+void funSalario(int x);
