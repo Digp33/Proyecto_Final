34011e33793a7bf02946752a90fa441d8c1497a1
Digp33 <85522284+Digp33@users.noreply.github.com>
2022-11-29 17:03:25 -0600

    Update main.c
    Las funciones se llaman siendo void

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "funciones.h"
 int main(int argc, char *argv[]) {
 	int selec;
-	int val;
+	
 	do
 	{
 		printf("Ingrese que desea obtener:\n1)Triangulo.c\n2)Cuadrado.c\n3)Conversiones\n4)Factorial de un numero\n5)Suma de matrices\n6)Multiplicación de matrices\n7)transpuesta de una matriz 3x3\n8)Salario\n9)Salir");
@@ -12,28 +12,28 @@ int main(int argc, char *argv[]) {
 		switch (selec)
 		{
 		case 1:
-			val= funTriangulo(selec);
+			funTriangulo(selec);
 			break;
 		case 2:
-			val=funCuadrilateros(selec);
+			funCuadrilateros(selec);
 			break;
 		case 3:
-			val= funConversion(selec);
+			funConversion(selec);
 			break;
 		case 4:
-			val=funFactorial(selec) ;
+			funFactorial(selec) ;
 			break;
 		case 5:
-			val=funSumaMatriz(selec);
+			funSumaMatriz(selec);
 			break;	
 		case 6:
-			val=funMultiMatriz(selec);
+			funMultiMatriz(selec);
 			break;	
 		case 7:
-			val=funTransMatriz(selec);
+			funTransMatriz(selec);
 			break;
 		case 8:
-			val=funSalario(selec);
+			funSalario(selec);
 			break;
 		case 9:
 			printf("Hasta Luego...\n");
